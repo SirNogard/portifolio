7bd24b7d4dd9bd6f5479cd22520a2995b4ba2af5
Renan Silva Ribeiro <76563704+SirNogard@users.noreply.github.com>
2024-06-30 22:36:51 -0300

    Delete projeto 2 directory

--- projeto 2/script.js
+++
@@ -1,6 +0,0 @@
-    function ToggleMenu() {
-        const menu = document.querySelector(".menu-links");
-        const icon = document.querySelector(".hamburguer-icon");
-        menu.classList.toggle("open");
-        icon.classList.toggle("open");
-    }
